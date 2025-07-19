c3d0694aba4ca4b079fa3203e2d38ff6f6d5de80
SoniaKawish <48656978+SoniaKawish@users.noreply.github.com>
2025-07-18 23:55:07 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.querySelector('.start-btn').addEventListener('click', () => {
   alert('Let’s get started!');
 });
+document.getElementById("menuToggle").addEventListener("click", () => {
+  document.getElementById("navMenu").classList.toggle("show");
+});
